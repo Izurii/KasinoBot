e44f5f88f33d1748d44e9de8035151919eb9984c
Heitor Augusto Massarente <heitor.massarente@h2ocompany.com.br>
2020-09-06 13:18:32 -0300

    Proper documentation.

--- database/index.js
+++ database/index.js
@@ -14,6 +14,12 @@ connection.connect(function(err) {
 	console.log('MySQL OK!');
 });
 
+const MysqlQueryType = connection.query;
+/**
+ * @description Function that execute a promise based query on a mysql db
+ * @param { MysqlQueryType } query 
+ * @returns { Object } - Returns a object containing the result of the query or a error in case.
+ */
 async function query(query) {
 	return new Promise((resolve, reject) => {
 		connection.query(query, (err, result) => {
