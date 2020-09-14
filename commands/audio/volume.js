ae0912f01b0d21ce8b9cdfecf798f1e4b0528754
Izurii <heitor_massarente@msn.com>
2020-09-14 13:49:27 -0300

    Fix volume command. Fix volume command description in help command.

--- commands/audio/volume.js
+++ commands/audio/volume.js
@@ -12,9 +12,15 @@ async function volume (message) {
 	let volume = message.content.split(" ");
 	volume = volume[1];
 
-	if(!volume.match('[0-9]')||volume > 5||volume==0)
-		return message.reply("Verifique se você digitou certo o volume cacete de cacuete. Pode ser de 1 a 10.");
+	if(volume==undefined||!volume.match('[0-9]')||volume > 5||volume==0)
+		return message.reply("Verifique se você digitou certo o volume cacete de cacuete. Pode ser de 1 a 5.");
 	try {
+		if(
+			serverQueue==undefined||
+			serverQueue.connection==undefined||
+			serverQueue.connection.dispatcher==undefined
+		)
+			return message.reply("Não t4 t0cand0 n4d4 irmã0 s3m mud4nç4s.");
 		serverQueue.connection.dispatcher.setVolume(volume);
 	} catch (error) {
 		console.log(error);
