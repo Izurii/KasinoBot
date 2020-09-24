f63316b6578f2bc6c4fccb28319bebed98a11b1a
Izurii <heitor_massarente@msn.com>
2020-09-24 15:34:42 -0300

    Adm message command created.

---
+++ commands/utility/adm_message.js
@@ -0,0 +1,25 @@
+const Controller = require('./utilityController');
+const DiscordMessageType = Controller.JSDocTypes.DiscordMessageType;
+const Bot = require('../../');
+exports.adm_message = adm_message;
+
+/**
+ * @description Function that sends a specific message to all guilds connected, only works with Izurii_Hootoh
+ * @param  { DiscordMessageType } message - Message that user sent to bot
+ */
+async function adm_message (message) { 
+	
+	const izht_id = '229096474762280960';
+
+	if(message.author.id!==izht_id)
+		return;
+
+	let messageToSend = message.content.substr(message.content.indexOf(' ')+1);
+	let guilds = await Bot.getAllGuilds();
+
+	guilds.forEach(server => {
+		let systemChannel = server.systemChannel;
+		systemChannel.send(messageToSend);
+	});
+
+}
