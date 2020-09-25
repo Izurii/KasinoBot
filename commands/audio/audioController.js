5503782d7140e8df1f969c0ed2adaa9c3ac141b5
Izurii <heitor_massarente@msn.com>
2020-09-24 21:28:49 -0300

    Fix.

--- commands/audio/audioController.js
+++ commands/audio/audioController.js
@@ -56,7 +56,7 @@ async function stopAllPlayUnique (message, music) {
 	voice_channels.forEach((channel) => { voiceChannel = channel; });
 
 	if (voiceChannel.length == 0)
-		return message.channe.send("Algu3m t3m que entr4r n4 v0z m3u");
+		return message.channel.send("Algu3m t3m que entr4r n4 v0z m3u");
 
 	const queueContruct = {
 		textChannel: message.channel,
