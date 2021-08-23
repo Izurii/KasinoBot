020a4bbdf9eaf2b00f49590c35b05db049b57bd1
Izurii <heitor_massarente@msn.com>
2021-08-23 18:00:46 -0300

    TO-FIX

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -2,8 +2,8 @@ import { getVoiceConnection } from '@discordjs/voice';
 import { IEventVoiceStateUpdate } from '../interfaces/IEventVoiceStateUpdate';
 
 export const run: IEventVoiceStateUpdate = async (oldState, newState) => {
-	if(newState.selfDeaf) {
-		const connection = getVoiceConnection(newState.guild.id);
-		connection?.destroy();
-	}
+	// if(newState.selfDeaf) {
+	// 	const connection = getVoiceConnection(newState.guild.id);
+	// 	connection?.destroy();
+	// }
 };
