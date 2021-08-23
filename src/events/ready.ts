ec128612805af080610c01f7bd70e903528d1de4
Izurii <heitor_massarente@msn.com>
2021-08-23 10:00:52 -0300

    Presence set

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,5 +1,8 @@
 import { IEventReady } from '../interfaces/IEventReady';
 
 export const run: IEventReady = async (client,) => {
+	
 	console.log(`Ready! ${client.readyAt}`);
+	client.user?.setPresence({ activities: [{ name: 'com seu cú | - help' }], status: 'online' });
+
 };
