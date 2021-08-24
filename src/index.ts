08c64fe5863b64fb1ab80c57b88863e87b8905f2
Izurii <heitor_massarente@msn.com>
2021-08-24 05:23:04 -0300

    Exit handler added

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,24 @@ require('dotenv').config();
 
 import { KasinoBot } from './client/Client';
 
-new KasinoBot().Start();
+const Kasino = new KasinoBot();
+Kasino.Start();
+
+process.stdin.resume();
+
+function exitHandler(options: { cleanup?: boolean, exit?: boolean }, exitCode: number) {
+	Kasino.destroy();
+	let exitMessage = '\nKasinoBot Logout!';
+	if (exitCode || exitCode === 0) exitMessage += ` Exit code: ${exitCode}`;
+	console.log(exitMessage);
+	if (options.exit) process.exit();
+}
+
+process.on('exit', exitHandler.bind(null, { cleanup:true }));
+
+process.on('SIGINT', exitHandler.bind(null, { exit:true }));
+
+process.on('SIGUSR1', exitHandler.bind(null, { exit:true }));
+process.on('SIGUSR2', exitHandler.bind(null, { exit:true }));
+
+process.on('uncaughtException', exitHandler.bind(null, { exit:true }));
