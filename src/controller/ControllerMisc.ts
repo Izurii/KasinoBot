2c9efe60cd37a5167ed0c4b630ff38771f943d6d
Izurii <heitor_massarente@msn.com>
2021-08-24 05:23:46 -0300

    Fixing return type and the return of the function.

--- src/controller/ControllerMisc.ts
+++ src/controller/ControllerMisc.ts
@@ -5,13 +5,16 @@ class ControllerMisc extends Controller {
 
 	public commandsPath = `${__dirname}/../commands/misc/*.ts`;
 
-	public async getValueCurrency(moedaOrigem: string, moedaDestino: string): Promise<number> {
-		return await this.axios.get(`https://economia.awesomeapi.com.br/all/${moedaOrigem}-${moedaDestino}`).then ((response: AxiosResponse) => {
+	public async getValueCurrency(moedaOrigem: string, moedaDestino: string): Promise<number|false> {
+		const result = await this.axios.get(`https://economia.awesomeapi.com.br/all/${moedaOrigem}-${moedaDestino}`).then ((response: AxiosResponse) => {
 			return parseFloat(response.data[moedaOrigem].ask).toFixed(2);
 		}).catch ((error: Error) => {
 			console.log(error);
 			return false;
 		});
+
+		if(!result) return false;
+		return result as unknown as number;
 	}
 
 }
